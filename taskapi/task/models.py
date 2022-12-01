b8711b6727cb25cfd3c19a8bcaeb45a550265312
bitscoder-boop <regdipesh113@gmail.com>
2022-12-01 21:49:56 +0545

    added directory path

--- taskapi/task/models.py
+++ taskapi/task/models.py
@@ -15,18 +15,22 @@ from datetime import datetime
 
 
 def user_directory_path(instance, filename: str):
-    new_name = str(filename) + datetime.now().strftime("%Y/%m/%d/%I_%M_%S_%f")
+    name, ext = filename.split('.')
+    # to make sure file is always unique
+    timestamps = datetime.now().strftime("%Y/%m/%d/%I_%M_%S_%f")
+    new_name = str(name) + timestamps + '.' + str(ext)
     # file will be uploaded to MEDIA_ROOT / user_<id>/<2022_11_01-19_55_22>
-    return 'user_{id}/{name}'.format(id=instance.user.id, name=new_name)
+    return 'user/{username}/{name}'.format(
+        username=instance.username,
+        name=new_name
+    )
 
 
 class User(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/' +
-                              str(user_directory_path))
-    file = models.FileField(upload_to='files/' +
-                            str(user_directory_path))
+    image = models.ImageField(upload_to=user_directory_path)
+    file = models.FileField(upload_to=user_directory_path)
 
     def __str__(self):
         return self.username
