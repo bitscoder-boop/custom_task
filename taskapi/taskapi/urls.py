1978ae1a05c915c0429f34cef5a337f78a5fc22d
bitscoder-boop <regdipesh113@gmail.com>
2022-12-01 21:18:10 +0545

    urls for the task app are added

--- taskapi/taskapi/urls.py
+++ taskapi/taskapi/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users', include('task.urls')),
     path('openapi', get_schema_view(
         title="Project Protofolio",
         description="Description",
