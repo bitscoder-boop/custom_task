e16bd40b8b095faf90cb87c7f4ac114389a48331
bitscoder-boop <regdipesh113@gmail.com>
2022-12-01 21:15:50 +0545

    swagger-docs and user model created

--- taskapi/task/models.py
+++ taskapi/task/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime
+
+# 1. Taking Model fields such as [ name, description, image, file ]
+# Create User
+# Perform CRUD operations and count the number of objects in the database.
+# Take a model field write it to the database and perform an automatic
+# delete operation.
+# Write Tests.
+# Dockerize
+# Write settings file for production level and local level.
+# Document it
 
 # Create your models here.
+
+
+def user_directory_path(instance, filename: str):
+    new_name = str(filename) + datetime.now().strftime("%Y/%m/%d/%I_%M_%S_%f")
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<2022_11_01-19_55_22>
+    return 'user_{id}/{name}'.format(id=instance.user.id, name=new_name)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/' +
+                              str(user_directory_path))
+    file = models.FileField(upload_to='files/' +
+                            str(user_directory_path))
+
+    def __str__(self):
+        return self.username
--- taskapi/taskapi/urls.py
+++ taskapi/taskapi/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('openapi', get_schema_view(
+        title="Project Protofolio",
+        description="Description",
+        version="1"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
